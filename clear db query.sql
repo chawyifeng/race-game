d68401f83ad1b180ecfbe5185a555ad89e6f17ca
Chaw Yi Feng <chawyifeng12@gmail.com>
2025-04-25 01:51:14 +0800

    remove sqllite db and rename sql query

---
+++ clear db query.sql
@@ -0,0 +1,13 @@
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- Drop the 'events' and 'events_days' tables
+DROP TABLE IF EXISTS `events`, `event_days`, `customers`, `admins`;
+
+-- Re-enable foreign key checks
+SET FOREIGN_KEY_CHECKS = 1;
+
+
+
+
+---------------
+SHOW TABLES;
