04c7bedce4a28e27a5c5a1cd8777ffb4800e5ac8
Chaw Yi Feng <chaw.yi.feng@mforce.my>
2025-04-30 09:31:17 +0800

    ADD INIT DB

---
+++ initDB.cjs
@@ -0,0 +1,18 @@
+// initDatabase.cjs
+const mysql = require('mysql2/promise');
+require('dotenv').config();
+
+const initDatabase = async () => {
+  const connection = await mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+  });
+
+  await connection.query(`CREATE DATABASE IF NOT EXISTS \`${process.env.DB_NAME}\``);
+  console.log(`Database '${process.env.DB_NAME}' created or already exists.`);
+
+  await connection.end();
+};
+
+module.exports = initDatabase;
